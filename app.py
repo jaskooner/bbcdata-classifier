48d00b24497d7eaef998c9a95c3547c2baf1a19b
scooter <jaskooner.singh@gmail.com>
2019-03-11 19:40:43 +0000

    cleaned up line endings in classifierdataset.csv

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import pandas as pd 
 
+dataset = "classifierdataset2.csv"
 nRowsRead = 1000
-df1 = pd.read_csv("bbc-dataset.csv", delimiter=",", nrows = nRowsRead)
+df1 = pd.read_csv(dataset, delimiter=",", nrows = nRowsRead)
 df1.dataframeName = " BBC news dataset"
 
 nRow, nCol = df1.shape
 print(f'There are {nRow} rows and {nCol} columns')
 
-# print(df1.loc[0]['tags'])
-
-df1.tags.replace(r"sports.+", "sports", regex=True, inplace=True)
-
-print(df1.loc[1]['tags'])
+print(df1['labels'])
